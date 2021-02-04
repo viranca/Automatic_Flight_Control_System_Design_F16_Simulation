cc93fa9b0f895418de23b2f9bc77810dd6dd81b6
viranca <37591728+viranca@users.noreply.github.com>
2021-02-04 02:59:18 +0100

    chapter 8 update

--- F16Sim v3/Chapter 7/GlideslopeController.m
+++ F16Sim v3/Chapter 7/GlideslopeController.m
@@ -14,4 +14,47 @@ sys_lo=ss(A_red_lo,B_red_lo,C_red_lo,D_red_lo);
 sim GlideslopeController_model
 
 
+
 %% Figures
+
+Altitude_plot = figure(1);
+hold on;
+plot(ans.Altitude{1}.Values(:))
+title('Altitude')
+xlabel('Time [s]');
+ylabel('Altitude [ft]');
+grid on
+
+
+Velocity_plot = figure(2);
+hold on;
+plot(ans.Velocity{1}.Values(:))
+title('Velocity')
+xlabel('Time [s]');
+ylabel('Velocity [ft]');
+grid on
+
+Alpha_plot = figure(3);
+hold on;
+plot(ans.Alpha{1}.Values(:))
+title('Alpha')
+xlabel('Time [s]');
+ylabel('Alpha [ft]');
+grid on
+
+Theta_plot = figure(4);
+hold on;
+plot(ans.Theta{1}.Values(:))
+title('Theta')
+xlabel('Time [s]');
+ylabel('Theta [ft]');
+grid on
+
+q_plot = figure(5);
+hold on;
+plot(ans.q{1}.Values(:))
+title('q')
+xlabel('Time [s]');
+ylabel('q [ft]');
+grid on
+
