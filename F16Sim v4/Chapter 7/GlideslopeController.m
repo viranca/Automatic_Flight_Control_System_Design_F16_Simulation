058de3e8c9afa5a3d073b0b0e95c9f9c6f0650b8
deniz <denizkalender@live.nl>
2021-02-06 15:09:31 +0100

    Gains update

--- F16Sim v4/Chapter 7/GlideslopeController.m
+++ F16Sim v4/Chapter 7/GlideslopeController.m
@@ -19,7 +19,7 @@ sys_lo=ss(A_red_lo,B_red_lo,C_red_lo,D_red_lo);
 Kq = -1
 Ktheta = 2
 Kv = 500
-Kglide = 50
+Kglide = 40
 
 
 
@@ -39,58 +39,58 @@ xlabel('Time [s]');
 ylabel('Altitude [ft]');
 grid on
 
-
-Velocity_plot = figure(2);
-hold on;
-plot(ans.Velocity{1}.Values(:))
-title('Velocity')
-xlabel('Time [s]');
-ylabel('Velocity [ft]');
-grid on
-
-Alpha_plot = figure(3);
-hold on;
-plot(ans.Alpha{1}.Values(:))
-title('Alpha')
-xlabel('Time [s]');
-ylabel('Alpha [ft]');
-grid on
-
-Theta_plot = figure(4);
-hold on;
-plot(ans.Theta{1}.Values(:))
-title('Theta')
-xlabel('Time [s]');
-ylabel('Theta [ft]');
-grid on
-
-q_plot = figure(5);
-hold on;
-plot(ans.q{1}.Values(:))
-title('q')
-xlabel('Time [s]');
-ylabel('q [ft]');
-grid on
-
-gamma_plot = figure(6);
-hold on;
-plot(ans.gamma{1}.Values(:))
-title('gamma + 3')
-xlabel('Time [s]');
-ylabel('gamma [ft]');
-grid on
-
-
-Gamma_plot = figure(7);
-hold on;
-plot(ans.Gamma{1}.Values(:))
-title('Gamma')
-xlabel('Time [s]');
-ylabel('Gamma [ft]');
-grid on
-
-
-
+% 
+% Velocity_plot = figure(2);
+% hold on;
+% plot(ans.Velocity{1}.Values(:))
+% title('Velocity')
+% xlabel('Time [s]');
+% ylabel('Velocity [ft]');
+% grid on
+% 
+% Alpha_plot = figure(3);
+% hold on;
+% plot(ans.Alpha{1}.Values(:))
+% title('Alpha')
+% xlabel('Time [s]');
+% ylabel('Alpha [ft]');
+% grid on
+% 
+% Theta_plot = figure(4);
+% hold on;
+% plot(ans.Theta{1}.Values(:))
+% title('Theta')
+% xlabel('Time [s]');
+% ylabel('Theta [ft]');
+% grid on
+% 
+% q_plot = figure(5);
+% hold on;
+% plot(ans.q{1}.Values(:))
+% title('q')
+% xlabel('Time [s]');
+% ylabel('q [ft]');
+% grid on
+% 
+% gamma_plot = figure(6);
+% hold on;
+% plot(ans.gamma{1}.Values(:))
+% title('gamma + 3')
+% xlabel('Time [s]');
+% ylabel('gamma [ft]');
+% grid on
+% 
+% 
+% Gamma_plot = figure(7);
+% hold on;
+% plot(ans.Gamma{1}.Values(:))
+% title('Gamma')
+% xlabel('Time [s]');
+% ylabel('Gamma [ft]');
+% grid on
+% 
+% 
+% 
 
 
 
--- F16Sim v5/Chapter 7/GlideslopeController.m
+++ F16Sim v5/Chapter 7/GlideslopeController.m
@@ -19,20 +19,19 @@ sys_lo=ss(A_red_lo,B_red_lo,C_red_lo,D_red_lo);
 Kq = -1
 Ktheta = 2
 Kv = 500
-Kglide = 50
+Kglide = 40
 
-Kflare = 1
-%hflare = 20
+%Kflare = 0.00005            % Kflare = 0.00005, Kglide = 40, Kv = 500, Ktheta = 2, Kq = -1
+Kflare = 0.00045  
 
-%Copy paste: % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-Initial_Speed = trim_state_lo(7) %300
-p = 8;                                      %Flare duration
-x_1 = 1100;                                 %Distance from glideslope transmitter to touchdown
-h_dot = -Initial_Speed*sin(degtorad(3))     %Speed at flare height
-x_2 = p*Initial_Speed-x_1                   %Distance from start of flare to glideslope transmitter
-hflare = tan(degtorad(3))*x_2              %Flare height
-tau=-hflare/h_dot                          %Time constant for flare geometry
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Flare height calculation
+x_1 = 1100;                          %[ft]    % Distance intended touchdown point to glideslope transmitter
+V_0 = trim_state_lo(7);              %[ft/s]  % Airspeed
+V_descent_t0 = V_0 * sin(-3*pi/180); %[ft/s]  % Descent rate at start of the manoeuvre
+tau = x_1/(4*V_0+(V_descent_t0/tan(3*pi/180)));%[s] flare geometry time variable 
+hflare = -V_descent_t0 * tau;       %[ft]    % Flare height
+
+
 
 
 
@@ -48,57 +47,104 @@ ylabel('Altitude [ft]');
 grid on
 
 
+
+Thrust_input_plot = figure(12);
+hold on;
+plot(ans.Thrust_input{1}.Values(:))
+title('Thrust_input')
+xlabel('Time [s]');
+ylabel('Thrust_input [ft]');
+grid on
+
+elevator_deflection_input_plot = figure(13);
+hold on;
+plot(ans.elevator_deflection_input{1}.Values(:))
+yline(3);
+title('elevator_deflection_input')
+xlabel('Time [s]');
+ylabel('elevator_deflection_input [ft]');
+grid on
+
+
+h_dot2_plot = figure(10);
+hold on;
+plot(ans.h_dot2{1}.Values(:))
+title('h_dot2')
+xlabel('Time [s]');
+ylabel('h_dot2 [ft]');
+grid on
+
+h_dot3_plot = figure(11);
+hold on;
+plot(ans.h_dot3{1}.Values(:))
+yline(3);
+title('h_dot3')
+xlabel('Time [s]');
+ylabel('h_dot3 [ft]');
+grid on
+
+
+Flare_plot = figure(8);
+hold on;
+plot(ans.Altitude{1}.Values(:))
+title('Altitude')
+xlabel('Time [s]');
+xlim([136 138])
+ylabel('Altitude [ft]');
+grid on
+
+
 Velocity_plot = figure(2);
 hold on;
 plot(ans.Velocity{1}.Values(:))
 title('Velocity')
 xlabel('Time [s]');
-ylabel('Velocity [ft]');
+ylabel('Velocity [ft/s]');
 grid on
 
-Alpha_plot = figure(3);
-hold on;
-plot(ans.Alpha{1}.Values(:))
-title('Alpha')
-xlabel('Time [s]');
-ylabel('Alpha [ft]');
-grid on
-
-Theta_plot = figure(4);
-hold on;
-plot(ans.Theta{1}.Values(:))
-title('Theta')
-xlabel('Time [s]');
-ylabel('Theta [ft]');
-grid on
-
-q_plot = figure(5);
-hold on;
-plot(ans.q{1}.Values(:))
-title('q')
-xlabel('Time [s]');
-ylabel('q [ft]');
-grid on
-
-gamma_plot = figure(6);
-hold on;
-plot(ans.gamma{1}.Values(:))
-title('gamma + 3')
-xlabel('Time [s]');
-ylabel('gamma [ft]');
-grid on
-
-
-Gamma_plot = figure(7);
-hold on;
-plot(ans.Gamma{1}.Values(:))
-title('Gamma')
-xlabel('Time [s]');
-ylabel('Gamma [ft]');
-grid on
-
-
-
+% Alpha_plot = figure(3);
+% hold on;
+% plot(ans.Alpha{1}.Values(:))
+% title('Alpha')
+% xlabel('Time [s]');
+% ylabel('Alpha [ft]');
+% grid on
+% 
+% Theta_plot = figure(4);
+% hold on;
+% plot(ans.Theta{1}.Values(:))
+% title('Theta')
+% xlabel('Time [s]');
+% ylabel('Theta [ft]');
+% grid on
+% 
+% q_plot = figure(5);
+% hold on;
+% plot(ans.q{1}.Values(:))
+% title('q')
+% xlabel('Time [s]');
+% ylabel('q [ft]');
+% grid on
+% 
+% gamma_plot = figure(6);
+% hold on;
+% plot(ans.gamma{1}.Values(:))
+% title('gamma + 3')
+% xlabel('Time [s]');
+% ylabel('gamma [ft]');
+% grid on
+% 
+% 
+% Gamma_plot = figure(7);
+% hold on;
+% plot(ans.Gamma{1}.Values(:))
+% title('Gamma')
+% xlabel('Time [s]');
+% ylabel('Gamma [ft]');
+% grid on
+% 
+% 
+% 
 
 
 
